91d4b4740370558f992e29db34e4db0732ecb43c
amtayaji002 <tyagiamit1975@yahoo.com>
2022-09-29 21:28:28 +0530

    Minor updates

--- at-00101-001-hubspoke-trial-basic/main.sh
+++ at-00101-001-hubspoke-trial-basic/main.sh
@@ -2,9 +2,24 @@
 
 mydatetimenow=$(date  -u +"%Y-%m-%dT%H:%M:%SZ")
 printf "INITIATED:PLAN: $mydatetimenow \n \n \n"
-# terraform plan -out=".output/tfplan-$mydatetimenow" | tee -a ".output/plan-$mydatetimenow.log"
-terraform plan -out=".output/tfplan-$mydatetimenow" | iconv --to-code=UTF-8 --output=".output/plan-$mydatetimenow.log" --verbose
-printf "OUTPUT:FILE: .output/tfplan-$mydatetimenow \n \n \n"
-printf "COMPLETED:PLAN: $mydatetimenow \n \n \n"
 
-# terraform show ".output/tfplan - $mydatetimenow"
+myoutfilename=".output/tfplan-$mydatetimenow.out"
+mylogfilename=".output/plan-$mydatetimenow.log"
+
+printf "myoutfilename = $myoutfilename \n \n"
+printf "mylogfilename = $mylogfilename \n \n"
+terraform plan -out "$myoutfilename" --json | tee -a "$mylogfilename"
+# terraform plan -out=".output/tfplan-$mydatetimenow" | tee -a ".output/plan-$mydatetimenow.log"
+# terraform plan -out=".output/tfplan-$mydatetimenow" | iconv --to-code=UTF-8 --output=".output/plan-$mydatetimenow.log" --verbose
+# terraform plan -out ".output/tfplan-$mydatetimenow".out | iconv --to-code=UTF-8 --output=".output/plan-$mydatetimenow.log" --verbose
+printf "COMPLETED:PLAN: $myoutfilename \n \n"
+
+read -p "Do you want to apply? (yes/no)?" doapply
+printf "\n \n"
+if [ "$doapply" = "yes" ]
+then
+    printf "INITIATED:APPLY: $myoutfilename \n \n"
+    terraform apply | tee -a "$mylogfilename-applied"
+    # terraform apply -state "$myoutfilename" | tee -a "$mylogfilename-applied"
+    printf "COMPLETED:APPLY: $myoutfilename \n \n"
+fi
--- at-00101-001-hubspoke-trial-basic/main.tf
+++ at-00101-001-hubspoke-trial-basic/main.tf
@@ -2,6 +2,7 @@
 # Azure Provider source and version being used.
 
 terraform {
+  required_version = ">= 1.3.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -24,7 +25,7 @@ data "azurerm_client_config" "core" {}
 
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "2.3.1"
+  version = "2.4.0"
 
   providers = {
     azurerm              = azurerm
