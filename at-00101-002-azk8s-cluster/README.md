16c25aaef25dd423fb50b4219022f47f3474afc5
amtayaji002 <tyagiamit1975@yahoo.com>
2022-10-02 04:15:40 +0530

    Added script for implementing a micro k8s cluster

---
+++ at-00101-002-azk8s-cluster/README.md
@@ -0,0 +1,7 @@
+
+|Sno  |Reference  |Reference URL  |
+|---------|---------|---------|
+|1     | QuickStart: Create a Kubernetes cluster with Azure Kubernetes Service using Terraform | https://learn.microsoft.com/en-us/azure/developer/terraform/create-k8s-cluster-with-tf-and-aks |
+|1     |         |         |
+|1     |         |         |
+|1     |         |         |
---
+++ at-00101-002-azk8s-cluster/terraform.tf
@@ -0,0 +1,20 @@
+# Configure Terraform to set the required AzureRM provider
+# version and features{} block.
+
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
